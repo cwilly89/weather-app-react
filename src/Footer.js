4d3b12df4cf4b5889fcb90642b368e56a82e4853
cwilly89 <88181324+cwilly89@users.noreply.github.com>
2021-10-23 23:20:15 -0700

    added the footer and css

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./footer.css";
+
+export default function Footer() {
+  return (
+    <footer>
+      <a href="https://github.com/cwilly89/weather-app-react">
+        Open sourced-code
+      </a>{" "}
+      by, Channell Mascaro
+    </footer>
+  );
+}
